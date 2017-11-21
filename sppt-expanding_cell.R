aeba4db0ae17fe89f6d5c481107ae4579e4c1927
Nick Malleson <n.s.malleson@leeds.ac.uk>
2017-11-21 11:38:25 +0100

    Initial commit to this branch. Have added the script that will run the expanding cell code (later this will be integrated into the sppt package properly)

---
+++ sppt-expanding_cell.R
@@ -0,0 +1,14 @@
+#' @import sppt
+NULL
+
+#' An extension to the sppt package that runs the test over a number of grids with varying
+#' resolutions.
+#' TODO - integrate this script into the sppt package properly.
+#' @param resolution.cutoff The point at which to stop. Once a grid with 'resolution.cutoff' number
+#' of cells has been created, the function will stop iterating.  
+
+sppt.exp.cell <- function(base_points.sp, test_points.sp, uoa.sp, resolution.cutoff = 1000,
+                          outputlist=FALSE, nsamples=200, percpoints=85, conf_level=95){
+    
+}
+                          
