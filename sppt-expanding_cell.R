38aba77345fba510feec65beca8e9ecf9c7757cc
Nick Malleson <n.s.malleson@leeds.ac.uk>
2018-04-03 13:02:30 +0100

    ?

--- sppt-expanding_cell.R
+++ sppt-expanding_cell.R
@@ -4,10 +4,12 @@ NULL
 #' An extension to the sppt package that runs the test over a number of grids with varying
 #' resolutions.
 #' TODO - integrate this script into the sppt package properly.
-#' @param resolution.cutoff The point at which to stop. Once a grid with 'resolution.cutoff' number
-#' of cells has been created, the function will stop iterating.  
-
-sppt.exp.cell <- function(base_points.sp, test_points.sp, uoa.sp, resolution.cutoff = 1000,
+#' @param min.area The minimum square area (in square meters) of the finest (highest resolution)
+#' grid.
+#' @param num.scales The number of times that the resolution should be increased. The smallest
+#' resolution will be equal to `min.area`, and the largest is one cell over the entire area.
+sppt.exp.cell <- function(base_points.sp, test_points.sp, uoa.sp, 
+                          min.area = 100, num.scales = 20,
                           outputlist=FALSE, nsamples=200, percpoints=85, conf_level=95){
     
 }
